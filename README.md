a7291816f3c0f4d1b4cb5bbd17cc3b947da7b0ca
JulesBelveze <jules.belveze@hotmail.fr>
2019-10-26 10:16:54 +0200

    chore: add preprocess file and README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# text-summarizer
+
+## Data 
+To get data, download the `CNN_STORIES_TOKENIZED` and `DM_STORIES_TOKENIZED` from [here](https://github.com/JafferWilson/Process-Data-of-CNN-DailyMail). Then create preprocess the data by running `preprocess_data.py`.
---
+++ preprocess_data.py
@@ -0,0 +1,58 @@
+import os
+import re
+from progress.bar import Bar
+from typing import List, Dict, Union
+from pickle import dump
+
+strip_regex = "|".join(['LRB', 'RRB', 'LSB', 'RSB'])
+
+
+def load_file(filename: str) -> str:
+    with open(filename, 'r', encoding='utf8') as f:
+        text = f.read()
+        return text
+
+
+def clean_story(story: str) -> str:
+    story = re.sub(strip_regex, " ", story)
+    story = re.sub(r"[,;@#?!&$\-\'\"\`]+\ *", " ", story)
+    story = " ".join(story.split())
+    story = story.lower()
+    return story
+
+
+def process_story(doc: str):
+    index = doc.find("@highlight")
+    story = doc[:index]
+    highlights = doc[index:].split("@highlight")
+    highlights = [h.strip() for h in highlights if len(h) > 0]
+    return story, highlights
+
+
+def load_stories(dir_name: str) -> List[Dict[str, Union[str, List[str]]]]:
+    data = []
+    list_files = os.listdir(dir_name)
+    bar = Bar("Processing data", max=len(list_files))
+    for file in list_files:
+        filename = os.path.join(dir_name, file)
+        doc = load_file(filename)
+        story, highlights = process_story(doc)
+        story = clean_story(story)
+        data.append({'story': story,
+                     "highlights": highlights})
+        bar.next()
+    bar.finish()
+    return data
+
+
+def save_pickle(filename: str, data: List[Dict[str, Union[str, List[str]]]]) -> None:
+    with open(filename, 'wb') as f:
+        dump(data, f)
+    print("{} created!\n".format(filename))
+
+
+if __name__ == "__main__":
+    dirs = ["data/cnn_stories_tokenized", "data/dm_stories_tokenized"]
+    for dir in dirs:
+        data = load_stories(dir)
+        save_pickle(dir + ".pkl", data)
